a5ea2deb7f89a11175c5b931863fd08dafa0bb51
jxj <2045927977@qq.com>
2025-05-05 21:55:16 +0800

    数据分析案例用功能包

---
+++ package/data_anysis/data_define.py
@@ -0,0 +1,11 @@
+"""
+数据定义的类
+"""
+
+class Record:
+
+    def __init__(self, data, order_id, money, province):
+        self.data = data
+        self.order_id = order_id
+        self.money = money
+        self.province = province
---
+++ package/data_anysis/file_define.py
@@ -0,0 +1,19 @@
+"""
+和文件相关的类定义
+"""
+
+from typing import List
+
+from package.data_anysis.data_define import Record
+
+
+class FileReader:
+
+    def read_data(self) -> List[Record]:
+        """
+        读取文件的数据
+        :return:
+        """
+        pass
+
+
