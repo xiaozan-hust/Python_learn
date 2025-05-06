77b2899fba89da782bbd7d4c4f223f618bc21212
jxj <2045927977@qq.com>
2025-05-06 11:43:32 +0800

    完善数据分析案例package

--- package/data_anysis/chart_define.py
+++ package/data_anysis/chart_define.py
@@ -1,66 +1,39 @@
 """
 和文件相关的类定义
 """
-import json
-from typing import List
-from package.data_anysis.data_define import Record
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+from pyecharts.globals import ThemeType
+from pyecharts.options import InitOpts, LabelOpts
 
 
-class FileReader:
+class VisualData:
 
-    def __init__(self, path):
-        """
-        记录待读取文件路径
-        :param path: 文件路径
-        """
-        self.path = path
+    def __init__(self, data: dict):
+        self.data: dict = data
 
-    def read_data(self) -> List[Record]:
-        """
-        读取文件的数据，将每一条数据都转换成Record对象，将它们都封装到list内返回
-        """
-        pass
+    def bar(self):
+        keys = list(self.data.keys())
+        values = list(self.data.values())
+        # 创建柱状图对象
+        # 写法一
+        # bar = (
+        #     Bar()
+        #     .add_xaxis(keys)
+        #     .add_yaxis("销售额", values)
+        #     .set_global_opts(
+        #         title_opts=opts.TitleOpts(title="日销售额可视化表"),
+        #         toolbox_opts=opts.ToolboxOpts(is_show=True)
+        #     )
+        # )
+        # 写法二
+        bar = Bar(init_opts=InitOpts(theme=ThemeType.LIGHT))
+        bar.add_xaxis( keys)
+        bar.add_yaxis("销售额", values, label_opts=LabelOpts(is_show=False))
+        bar.set_global_opts(
+            title_opts=opts.TitleOpts(title="日销售额可视化表"),
+            toolbox_opts=opts.ToolboxOpts(is_show=True)
+        )
 
-class TextFileReader(FileReader):
-
-    def read_data(self) -> List[Record]:
-        f = open(self.path, "r", encoding="UTF-8")
-        lines = f.readlines()
-        records = list()
-        for line in lines:
-            line = line.strip()     # 消除每一行的\n
-            line = line.split(",")  # 注意这里是英文逗号
-            record = Record(line[0], line[1], line[2], line[3])
-            records.append(record)
-            print(record)
-        f.close()
-        return records
-
-class JSONFileReader(FileReader):
-
-    def read_data(self) -> List[Record]:
-        f = open(self.path, "r", encoding="UTF-8")
-        lines = f.readlines()
-        records = list()
-        for line in lines:
-            record = json.loads(line)   # 此处JSON中的数据被解析为字典类型
-            record = Record(record["date"], record["order_id"], record["money"], record["province"])
-            records.append(record)
-            print(record)
-        f.close()
-        return records
-
-def get_data(reader: FileReader) -> List[Record]:
-    return reader.read_data()
-
-if __name__ == "__main__":
-    # file_path = "../../file/2011年1月销售数据.txt"
-    # file_path = "../../file/2011年2月销售数据JSON.txt"
-    # text_reader = TextFileReader(file_path)
-    # json_reader = JSONFileReader(file_path)
-    # text_reader.read_data()
-    # json_reader.read_data()
-    # 基于多态的方式实现数据读取
-    file_path = "../../file/2011年1月销售数据.txt"
-    text_reader = TextFileReader(file_path)
-    data_list = get_data(text_reader)
+        # 渲染图表到 HTML 文件
+        bar.render("../charts/bar_chart.html")
--- package/data_anysis/data_define.py
+++ package/data_anysis/data_define.py
@@ -1,11 +1,44 @@
 """
 数据定义的类
 """
+from typing import List
+
 
 class Record:
 
-    def __init__(self, data, order_id, money, province):
-        self.data = data
+    def __init__(self, date, order_id, money, province):
+        self.date = date
         self.order_id = order_id
         self.money = money
         self.province = province
+
+    def __str__(self):
+        # print(f"日期为：{self.date}，ID为：{self.order_id}，金额为：{self.money}，省份为：{self.province}")
+        return f"日期为：{self.date}，ID为：{self.order_id}，金额为：{self.money}，省份为：{self.province}"
+
+class ProcessDate:
+
+    def __init__(self, dataset):
+        self.dataset: List[Record] = dataset
+
+    def reshape_by_data(self) -> dict:
+        data_by_day = dict()
+        # 该思路不够优雅
+        # for i in range(1, 3):
+        #     date_month = "2011-0" + str(i)
+        #     for j in range(1, 32):
+        #         if j < 10:
+        #             data_day = date_month + "-0" + str(j)
+        #         else:
+        #             data_day = date_month + "-" + str(j)
+        #         data_by_day[data_day] = 0
+        #         for data in self.dataset:
+        #             if data_day == data.date:
+        #                 data_by_day[data_day] += int(data.money)
+        # 推荐使用该思路
+        for data in self.dataset:
+            if data.date in data_by_day.keys():
+                data_by_day[data.date] += int(data.money)
+            else:
+                data_by_day[data.date] = int(data.money)
+        return data_by_day
